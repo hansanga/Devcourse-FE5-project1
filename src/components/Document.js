0a2eba266af2b74c174a86ffe5e6e39158d3b5a9
ygvbhy <ygvbhy@naver.com>
2025-04-03 12:36:30 +0900

    Feat: Document 페이지 삭제 이벤트 연결

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,5 +1,13 @@
-export default function Document({ $app, initialState }) {
+export default function Document({
+  $app,
+  initialState,
+  handleDelete,
+  handleSave,
+}) {
   this.state = initialState;
+
+  this.handleDelete = handleDelete;
+  this.handleSave = handleSave;
 
   this.$target = document.createElement("div");
   this.$target.className = "editor";
@@ -8,16 +16,42 @@ export default function Document({ $app, initialState }) {
   this.template = () => {
     let temp = `
         <div class="header">
-          <div class="pageTree"></div>
-          <button clss="pageDelete">삭제</button>
+          <div class="pageTree">새 페이지 / 하위 페이지 1</div>
+          <button id="pageDelete"><span class="material-symbols-outlined">delete</span></button>
         </div>
-        <div class="content"></div>
+        <div class="content">
+          <div class="documentTitle" contenteditable="true" placeholder="새 페이지 제목을 입력해주세요."></div>
+          <div class="documentContent" contenteditable="true" placeholder="내용을 입력해주세요."></div>
+        </div>
     `;
     return temp;
   };
 
   this.render = () => {
     this.$target.innerHTML = this.template();
+
+    this.$target.querySelector("#pageDelete").addEventListener("click", (e) => {
+      e.preventDefault();
+      this.handleDelete(); // id 값 추가
+    });
+
+    this.$target
+      .querySelector(".documentContent")
+      .addEventListener("input", (e) => {
+        e.preventDefault();
+        this.handleSave({
+          content: e.target.innerText,
+        });
+      });
+
+    this.$target
+      .querySelector(".documentTitle")
+      .addEventListener("input", (e) => {
+        e.preventDefault();
+        this.handleSave({
+          title: e.target.innerText,
+        });
+      });
   };
 
   this.setState = (newState) => {
